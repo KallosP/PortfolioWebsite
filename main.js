6d6b07d92d0493a7df1035024ba682ac8b41c13b
KallosP <97070073+KallosP@users.noreply.github.com>
2024-03-26 18:48:59 -0700

    added contact section

---
+++ main.js
@@ -0,0 +1,22 @@
+// window.onload = waits for the window to fully load, then executes the javascript code
+window.onload = function () {
+    // A reference to all elements with input-field class
+    var inputField = document.getElementsByClassName('input-field');
+
+    // Add event listeners to all elements
+    for (var i = 0; i < inputField.length; ++i) {
+
+        // Remove the pulseBox class from all elements with input-field class
+        inputField[i].addEventListener('focus', function () {
+            this.classList.remove('pulseBox')
+            this.style.backgroundColor = 'transparent';
+        });
+
+        // Re-add the pulseBox class
+        inputField[i].addEventListener('blur', function () {
+            this.classList.add('pulseBox')
+            this.style.backgroundColor = 'rgba(28, 255, 134, 0.151)';
+        });
+
+    }
+}
